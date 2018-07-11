9e5deb4cadffa10756af6d0d8cd8fb930c32d398
Dawid Czartoszewski <dawid.czartoszewski@gmail.com>
2018-07-11 11:26:45 +0200

    added label question

--- src/pl/akademiakodu/Main.java
+++ src/pl/akademiakodu/Main.java
@@ -1,14 +1,37 @@
 package pl.akademiakodu;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends JFrame {
+
+
+
+    private JButton buttonYes;
+    private JButton buttonNo;
+    private JLabel labelQuestion;
+
+    public Main(){
+        super("Milionerzy");
+        setSize(500,500);
+        setDefaultCloseOperation(1);//sprawia ze dziala przycisk exit
+        setVisible(true);//
+        buttonYes =new JButton("Tak");
+        buttonNo =new JButton("Nie");
+        labelQuestion = new JLabel("Czy Polska leży w Europie",0);
+        add(buttonYes);
+        add(buttonNo);
+        add(labelQuestion);
+0   }
+
+
+
 
     public static void main(String[] args) {
     List<Question> questionList =new ArrayList<>();
-
+    Main main = new Main();
     questionList.add(new Question("Czy Polska leży w europie",true));
     questionList.add(new Question("Czy 2+5 =8",false));
     questionList.add(new Question("czy 2^10=1024",true));
